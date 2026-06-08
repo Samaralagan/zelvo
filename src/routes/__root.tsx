fca7cbf7d781be98b145e25083a0ef75f5d1ea20
samaralagan <samaralagannthayaparan@gmail.com>
2026-06-08 21:18:41 +0530

    Logo updated1

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -7,6 +7,7 @@ import {
   HeadContent,
   Scripts,
 } from "@tanstack/react-router";
+import { Analytics } from "@vercel/analytics/react";
 
 import appCss from "../styles.css?url";
 
@@ -119,6 +120,7 @@ function RootComponent() {
   return (
     <QueryClientProvider client={queryClient}>
       <Outlet />
+      <Analytics />
     </QueryClientProvider>
   );
 }
